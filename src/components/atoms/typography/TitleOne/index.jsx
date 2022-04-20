b7e5c72f0e2f9d581189615dc712ce0241b3e7e2
Honeysalad <oliveira.douglasedu@outlook.com>
2022-04-20 00:56:02 -0300

    creates title typography

---
+++ src/components/atoms/typography/TitleOne/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.scss";
+
+const TitleOne = ({ children }) => {
+  return <span className="title-one bright align-right">{children}</span>;
+};
+
+export default TitleOne;
