e66e0a60b3b5e7a329532a4e87c8db993b2611a1
Honeysalad <oliveira.douglasedu@outlook.com>
2022-04-19 23:43:38 -0300

    creates cross svg

---
+++ src/components/svgs/Cross/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Cross = ({ color }) => {
+  return (
+    <svg
+      width="15"
+      height="16"
+      viewBox="0 0 15 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M3.95508 0.150391L7.42676 5.92188L10.9424 0.150391H14.1211L8.93555 7.97266L14.2822 16H11.1475L7.48535 10.0527L3.82324 16H0.673828L6.00586 7.97266L0.834961 0.150391H3.95508Z"
+        fill={color}
+      />
+    </svg>
+  );
+};
+
+Cross.defaultProps = {
+  color: "black",
+};
+
+export default Cross;
