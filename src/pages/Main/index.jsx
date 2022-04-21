30f8f6d9945d457b7e0b64c883d0c252d61f2df2
Honeysalad <oliveira.douglasedu@outlook.com>
2022-04-21 17:53:44 -0300

    starts creating info section

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,29 +1,63 @@
 import React from "react";
 import { WideButton } from "../../components/atoms/Buttons";
-import { Text, TitleTwo } from "../../components/atoms/typography";
+import {
+  Text,
+  TitleOne,
+  TitleThree,
+  TitleTwo,
+} from "../../components/atoms/typography";
 import { Arrow, LogoPortuguese } from "../../components/svgs";
 import { AlignEnum } from "../../enums";
 
 import "./styles.scss";
 
 const Main = () => {
+  const scrollTo = (target) => {
+    document.getElementById(target).scrollIntoView();
+  };
+
   return (
     <div className="main">
       <section className="splash">
         <div />
         <LogoPortuguese />
-        <WideButton compressed>
+        <WideButton onClick={() => scrollTo("info")} compressed>
           <Arrow />
         </WideButton>
       </section>
-      <section className="info">
-        <TitleTwo align={AlignEnum.RIGHT}>O QUÊ?</TitleTwo>
-        <Text align={AlignEnum.JUSTIFY}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
-          obcaecati culpa. Modi, optio amet assumenda error possimus, corrupti
-          natus eius voluptates eligendi sunt, at accusantium labore? Sunt
-          aliquam beatae pariatur.
-        </Text>
+      <section className="info" id="info">
+        <div className="content">
+          <TitleOne align={AlignEnum.RIGHT} textMargin>
+            `17-`21
+          </TitleOne>
+          <TitleTwo align={AlignEnum.RIGHT} textMargin>
+            O QUÊ?
+          </TitleTwo>
+          <Text align={AlignEnum.JUSTIFY} textMargin>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Perspiciatis, obcaecati culpa. Modi, optio amet assumenda error
+            possimus, corrupti natus eius voluptates eligendi sunt, at
+            accusantium labore? Sunt aliquam beatae pariatur.
+          </Text>
+          <TitleTwo align={AlignEnum.RIGHT} textMargin>
+            AGORA VAI
+          </TitleTwo>
+          <Text align={AlignEnum.JUSTIFY} textMargin>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Perspiciatis, obcaecati culpa. Modi, optio amet assumenda error
+            possimus, corrupti natus eius voluptates eligendi sunt, at
+            accusantium labore? Sunt aliquam beatae pariatur.
+          </Text>
+          <TitleThree align={AlignEnum.RIGHT} textMargin>
+            SERÁ QUE FOI?
+          </TitleThree>
+          <Text align={AlignEnum.JUSTIFY} textMargin>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Perspiciatis, obcaecati culpa. Modi, optio amet assumenda error
+            possimus, corrupti natus eius voluptates eligendi sunt, at
+            accusantium labore? Sunt aliquam beatae pariatur.
+          </Text>
+        </div>
       </section>
     </div>
   );
