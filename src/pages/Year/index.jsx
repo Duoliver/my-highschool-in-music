d34e172fcf4452f2ef644cbd15f6795ce676e999
Duoliver <oliveira.douglasedu@outlook.com>
2022-04-22 13:03:08 -0300

    feat(year): create teste database and integrates

--- src/pages/Year/index.jsx
+++ src/pages/Year/index.jsx
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { TitleOne } from "../../components/atoms/typography";
+import { Text, TitleOne, TitleTwo } from "../../components/atoms/typography";
 import { YearButtons } from "../../components/molecules";
 import { AlignEnum } from "../../enums";
+import database from "../../database";
 
 import "./styles.scss";
+import { YearData } from "../../database/classes";
 
 const Year = () => {
   const { yearId } = useParams();
+
+  const [data, setData] = useState(new YearData("", "", "", []));
+
+  useEffect(() => {
+    if (yearId) {
+      setData(database[yearId]);
+    }
+  }, [yearId]);
 
   return (
     <main className="year">
       <section className="info">
         <div className="content">
           <TitleOne align={AlignEnum.RIGHT} textMargin>
-            {`20${yearId}`}
+            {data.year}
           </TitleOne>
+          <TitleTwo align={AlignEnum.RIGHT} textMargin>
+            {data.title}
+          </TitleTwo>
+          <Text align={AlignEnum.JUSTIFY} textMargin>
+            {data.text}
+          </Text>
         </div>
         <div className="content">
           <YearButtons current={yearId} />
