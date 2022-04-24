379764c88b91953cdd761bea748e716b3592d303
Duoliver <oliveira.douglasedu@outlook.com>
2022-04-24 15:00:59 -0300

    feat(album): start creating inner modal
    to do: internal scrolling

--- src/components/atoms/Modal/index.jsx
+++ src/components/atoms/Modal/index.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { SmallButtonVariantEnum } from "../../../enums";
+import { SmallButton } from "../Buttons";
 
 import "./styles.scss";
 
 const Modal = ({ title, children, onClose }) => {
-  return <div className="modal-outer" onClick={onClose}></div>;
+  return (
+    <div className="modal-outer" onClick={onClose}>
+      <div className="modal-content">
+        <div className="modal-content-header default-padding">
+          {title}
+          <SmallButton
+            variant={SmallButtonVariantEnum.CROSS}
+            onClick={onClose}
+          />
+        </div>
+        <div className="modal-content-main">
+          <div className="default-padding">{children}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
