8cb480479661d17e8748273eeec26d170f2d03e3
Duoliver <oliveira.douglasedu@outlook.com>
2022-04-25 00:53:56 -0300

    feat(album): fix modal max width

--- src/components/atoms/Modal/index.jsx
+++ src/components/atoms/Modal/index.jsx
@@ -7,7 +7,7 @@ import "./styles.scss";
 const Modal = ({ title, children, onClose }) => {
   return (
     <div className="modal-outer" onClick={onClose}>
-      <div className="modal-content">
+      <div className="modal-content content">
         <div className="modal-content-header default-padding">
           {title}
           <SmallButton
