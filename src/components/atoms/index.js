7bf9eef80c6c83d1b040767b5dbaf14fa0235ca9
Duoliver <oliveira.douglasedu@outlook.com>
2022-04-27 20:57:28 -0300

    feat(album): add general atoms export

---
+++ src/components/atoms/index.js
@@ -0,0 +1,17 @@
+import AlbumCover from "./AlbumCover";
+import { SmallButton, SquareButton, WideButton } from "./Buttons";
+import Modal from "./Modal";
+import { Credits, Text, TitleOne, TitleTwo, TitleThree } from "./typography";
+
+export {
+  AlbumCover,
+  SmallButton,
+  SquareButton,
+  WideButton,
+  Modal,
+  Credits,
+  Text,
+  TitleOne,
+  TitleTwo,
+  TitleThree,
+};
