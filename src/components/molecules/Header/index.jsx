4934b9f0c8995b7dbb8530afe32168408ae6b3c5
Duoliver <oliveira.douglasedu@outlook.com>
2022-04-30 00:48:38 -0300

    feat(header): create navigation and responsivity

--- src/components/molecules/Header/index.jsx
+++ src/components/molecules/Header/index.jsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { SmallButtonVariantEnum } from "../../../enums";
+import routes from "../../../routes";
 import { SmallButton } from "../../atoms/Buttons";
 import { TitleTwo } from "../../atoms/typography";
 import "./styles.scss";
 
 const Header = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const getButton = useCallback(() => {
+    if (location.pathname === routes.MAIN) {
+      return (
+        <SmallButton
+          variant={SmallButtonVariantEnum.PLAY}
+          onClick={() => navigate(`${routes.YEAR_ROOT}/17`)}
+        />
+      );
+    }
+    return (
+      <SmallButton
+        variant={SmallButtonVariantEnum.ARROW_LEFT}
+        onClick={() => navigate(routes.MAIN)}
+      />
+    );
+  }, [location.pathname, navigate]);
+
   return (
-    <header className="header">
-      <SmallButton variant={SmallButtonVariantEnum.PLAY} />
-      <TitleTwo uppercase>Meu Colegial Em Música 2017-2021/1</TitleTwo>
+    <header className="header" id="header">
+      {getButton()}
+      <TitleTwo className="header-first-title" uppercase>
+        Meu Colegial Em Música 2017-2021/1
+      </TitleTwo>
+      <TitleTwo className="header-second-title">`17-`21</TitleTwo>
     </header>
   );
 };
