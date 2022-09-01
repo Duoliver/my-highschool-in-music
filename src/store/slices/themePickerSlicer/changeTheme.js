f4228661be72719810a174552b0933e12fbc3894
Duoliver <oliveira.douglasedu@outlook.com>
2022-09-01 20:37:59 -0300

    feat(dark): change overlay and status bar colour by theme

--- src/store/slices/themePickerSlicer/changeTheme.js
+++ src/store/slices/themePickerSlicer/changeTheme.js
@@ -1,6 +1,7 @@
 import { ThemeEnum } from "../../../enums";
 
 const html = document.querySelector("html");
+const metaThemeColour = document.querySelector("meta[name=theme-color]");
 
 const getStyle = (styleName) =>
   window.getComputedStyle(html).getPropertyValue(styleName);
@@ -8,11 +9,13 @@ const getStyle = (styleName) =>
 const defaultTheme = {
   background: getStyle("--background"),
   color: getStyle("--color"),
+  overlay: getStyle("--overlay"),
 };
 
 const darkTheme = {
   background: "black",
   color: "white",
+  overlay: "#FFFFFF80",
 };
 
 const themes = {
@@ -22,6 +25,7 @@ const themes = {
 
 const changeTheme = (themeName) => {
   const colours = themes[themeName] || {};
+  metaThemeColour.setAttribute("content", colours.background);
   Object.keys(colours).map((key) =>
     html.style.setProperty(`--${key}`, colours[key])
   );
